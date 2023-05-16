fc18ab93ed169714fe26ec8740ee602dd97dd325
ray cutler <ray@vidvox.net>
2023-05-16 16:57:54 -0400

    ISFMSLKit.framework is a target in the VVMetalKit project, which is its only dependency.  This commit also adds an external build target to the project, which is used to build libISFGLSLGenerator via cmake and places its build products in the source tree.

---
+++ ISFGLSLGenerator_build_script.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# this script needs to be run from xcode, which supplies the env vars used below.
+
+echo "Processing ISFGLSLGenerator external build script..."
+
+# this is the directory we're telling cmake to build
+LOCAL_SRC_DIR="${PROJECT_DIR}/submodules/ISFMSLKit/submodules/ISFGLSLGenerator"
+
+# we're going to make this directory, and put all the cmake files for this project in it
+LOCAL_BUILD_DIR="${PROJECT_TEMP_ROOT}/ISFGLSLGenerator.build"
+
+# make sure that cmake has created a build system in the build dir (if we don't, the 'clean' command may fail)
+/opt/homebrew/bin/cmake -S "${LOCAL_SRC_DIR}" -B "${LOCAL_BUILD_DIR}"
+
+# this is a "flag file"- its contents are irrelevant, the file's existence indicates state
+LOCAL_BUILD_FILE_FLAG="${TARGET_BUILD_DIR}/ISFGLSLGeneratorPreventCleanBuild"
+# if the file at this path does NOT exist, this is either a first-time build or the user chose to do a clean build (which deleted the file).  in either case, force a clean build immediately!
+if [ ! -f $LOCAL_BUILD_FILE_FLAG ] || [ "${ACTION}" = "clean" ]
+then
+	echo "performing a clean..."
+	/opt/homebrew/bin/cmake --build "${LOCAL_BUILD_DIR}" --target clean
+	echo "1">"${LOCAL_BUILD_FILE_FLAG}"
+fi
+
+# if this is a clean action, we're done- return before we build...
+if [ "${ACTION}" = "clean" ]
+then
+	echo "bailing, clean complete..."
+	exit 0
+fi
+
+echo "buliding ISFGLSLGenerator..."
+
+/opt/homebrew/bin/cmake --build "${LOCAL_BUILD_DIR}" --config RelWithDebInfo
+/opt/homebrew/bin/cmake --install "${LOCAL_BUILD_DIR}" --prefix "${LOCAL_SRC_DIR}/build/install"
